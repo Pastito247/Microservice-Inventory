1dbac293636fd672b65ec78db907118e0f92a62a
Pasto247 <117881893+Pastito247@users.noreply.github.com>
2025-04-25 11:01:32 -0400

    Add Proyect SpringBoot

---
+++ microservice-inventory/src/test/java/com/microservice/inventory/microservice_inventory/MicroserviceInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.inventory.microservice_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
